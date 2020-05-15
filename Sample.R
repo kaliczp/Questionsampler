e14c8c43a876c26185c4bbcede47a64862f51ae1
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-05-15 11:28:08 +0200

    Initial commit

---
+++ Sample.R
@@ -0,0 +1,15 @@
+sample(c(1 # 1. Tétel
+        ,2 # 2. Tétel
+        ,3 # 3. Tétel
+        ,4 # 4. Tétel
+        ,5 # 5. Tétel
+        ,6 # 6. Tétel
+        ,7 # 7. Tétel
+        ,8 # 8. Tétel
+        ,9 # 9. Tétel
+        ,10 # 10. Tétel
+        ,11 # 11. Tétel
+        ,12 # 12. Tétel
+        ,13 # 13. Tétel
+        ,14 # 14. Tétel
+        ), 1)
